38a48fe3e18f674aa349de54320643574bc83687
prit <132353280+Pritpatel11@users.noreply.github.com>
2025-09-29 19:53:24 +0530

    Update LoginActivity.kt

--- app/src/main/java/com/example/p1/LoginActivity.kt
+++ app/src/main/java/com/example/p1/LoginActivity.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -24,7 +23,7 @@ class LoginActivity : AppCompatActivity() {
 
         val singupbtn = findViewById<TextView>(R.id.Text_5)
         singupbtn.setOnClickListener {
-            val intent = Intent(this, Register::class.java)
+            val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
 
